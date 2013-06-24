a0054e1279b24ff4d8d337a31ce8f15ff5c3a520
Mihai Mogosanu <mike@sapiensworks.com>
2013-06-24 13:36:45 +0300

    starting experiments

--- src/SqlFu/Properties/AssemblyInfo.cs
+++ src/SqlFu/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyVersion("1.4")]
+[assembly: AssemblyFileVersion("1.4")]
 [assembly: InternalsVisibleTo("Tests")]
