a278239e21c14beb1e17990dce18b46104761752
Mihai Mogosanu <mike@sapiensworks.com>
2013-09-20 13:09:36 +0300

    added ext method to search migrations in current assembly

--- src/SqlFu/Properties/AssemblyInfo.cs
+++ src/SqlFu/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyVersion("2.2")]
+[assembly: AssemblyFileVersion("2.2")]
 [assembly: InternalsVisibleTo("Tests")]
