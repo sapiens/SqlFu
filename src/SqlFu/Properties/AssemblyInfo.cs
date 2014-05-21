722fb631cb33df385dae232935ad361e920155ca
Mihai Mogosanu <mike@sapiensworks.com>
2014-05-20 20:27:34 -0400

    updated to work latest version of caveman tools

--- src/SqlFu/Properties/AssemblyInfo.cs
+++ src/SqlFu/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
 [assembly: InternalsVisibleTo("Tests")] 
