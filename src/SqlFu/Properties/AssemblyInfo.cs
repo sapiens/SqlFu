a2047f559fe76b245ac8641bcb00a500b30c57c0
Mihai Mogosanu <mike@sapiensworks.com>
2014-06-23 19:42:25 -0400

    fixed wrong nuget dep versions

--- src/SqlFu/Properties/AssemblyInfo.cs
+++ src/SqlFu/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("2.3.2.0")]
-[assembly: AssemblyFileVersion("2.3.2.0")]
+[assembly: AssemblyVersion("2.3.3.0")]
+[assembly: AssemblyFileVersion("2.3.3.0")]
 [assembly: InternalsVisibleTo("Tests")]
