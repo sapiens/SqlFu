cce04ffa081e09369c63a54fc6337529ec7ffd57
Mihai Mogosanu <mike@sapiensworks.com>
2015-11-02 10:43:41 -0500

    updated nuspec

--- src/SqlFu/Properties/AssemblyInfo.cs
+++ src/SqlFu/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SqlFu")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
