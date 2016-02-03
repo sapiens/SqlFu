79b0e90db02a1974ab20d85954056db5e51a97e5
Mihai Mogosanu <mike@sapiensworks.com>
2016-02-02 20:54:47 -0500

    added docs to resilience methods

--- src/SqlFu/Extensions/TransientResilienceExtensions.cs
+++ src/SqlFu/Extensions/TransientResilienceExtensions.cs
@@ -8,7 +8,15 @@ namespace SqlFu
 {
     public static class TransientResilienceExtensions
     {
-
+        /// <summary>
+        /// Wraps db access action and retries it for 10 times with a slight delay between retries
+        /// if the db is temporary unavailable (timeout or connection limit reached)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbFactory"></param>
+        /// <param name="action"></param>
+        /// <exception cref="PersistenceException"></exception>
+        /// <returns></returns>
         public static T Do<T>(this IDbFactory dbFactory,Func<DbConnection,T> action)
         {
             T result = default(T);
@@ -27,6 +35,13 @@ namespace SqlFu
             return result;
         }
 
+        /// <summary>
+        /// Wraps db access action and retries it for 10 times with a slight delay between retries
+        /// if the db is temporary unavailable (timeout or connection limit reached)
+        /// </summary>
+        /// <param name="dbFactory"></param>
+        /// <exception cref="PersistenceException"></exception>
+        /// <param name="action"></param>
         public static void Do(this IDbFactory dbFactory, Action<DbConnection> action)
         {
             ModelTools.RetryOnException<DbException>(() =>
@@ -43,7 +58,17 @@ namespace SqlFu
 
             });
         }
-    
+
+        /// <summary>
+        /// Wraps db access action and retries it for 10 times with a slight delay between retries
+        /// if the db is temporary unavailable (timeout or connection limit reached)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbFactory"></param>
+        /// <param name="token"></param>
+        /// <param name="action"></param>
+        /// <exception cref="PersistenceException"></exception>
+        /// <returns></returns>
         public static async Task<T> DoAsync<T>(this IDbFactory dbFactory, CancellationToken token,Func<DbConnection,CancellationToken,Task<T>> action)
         {
             T result = default(T);
@@ -63,6 +88,16 @@ namespace SqlFu
             return result;
         }
 
+
+        /// <summary>
+        /// Wraps db access action and retries it for 10 times with a slight delay between retries
+        /// if the db is temporary unavailable (timeout or connection limit reached)
+        /// </summary>
+        /// <param name="dbFactory"></param>
+        /// <param name="token"></param>
+        /// <param name="action"></param>
+        /// <exception cref="PersistenceException"></exception>
+        /// <returns></returns>
         public static async Task DoAsync(this IDbFactory dbFactory, CancellationToken token,Func<DbConnection,CancellationToken,Task> action)
         {
             await ModelTools.RetryOnException<DbException>(async () =>
