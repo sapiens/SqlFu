56d5fb9cdce399b02df7420fdda22367c30f3ff2
Mihai Mogosanu <mike@sapiensworks.com>
2016-02-06 11:38:21 -0500

    added build scripts

--- build/build.cmd
+++ build/build.cmd
@@ -1,5 +1,7 @@
 @echo off
 cls
+
 rem "tools\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
+rem "tools\nuget.exe" "install" "Newtonsoft.Json" "-OutputDirectory" "tools" "-ExcludeVersion"
 "tools\FAKE\tools\Fake.exe" build.fsx %*
 pause
