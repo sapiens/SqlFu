2b50f0b49f7841f54ea681183410f2b7418c8536
Mihai Mogosanu <mike@sapiensworks.com>
2016-07-15 10:07:29 -0400

    updated to Core RTM

--- src/SqlFu/Properties/AssemblyInfo.cs
+++ src/SqlFu/Properties/AssemblyInfo.cs
@@ -18,8 +18,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("84d33301-d97f-4dc1-a667-d95ccf8a58fa")]
 
 // Version information for an assembly consists of the following four values:
 //
