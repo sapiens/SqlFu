ed690a24b9d18baf81a185f2013a616c22c0481b
Mihai Mogosanu <mike@sapiensworks.com>
2017-09-29 13:26:10 -0400

    added helper method for creating storages

---
+++ src/SqlFu/InitStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Common;
+using SqlFu.Providers;
+
+namespace SqlFu
+{
+    public static class InitStorage
+    {
+        public static void CreateStorage(this DbConnection cnx,Func<IEscapeIdentifier,string> getSql,Action<SqlFuConfig> config=null)
+        {
+            config?.Invoke(cnx.SqlFuConfig());
+            cnx.AddDbObjectOrIgnore(getSql(cnx.Provider()));
+        }
+    }
+}
