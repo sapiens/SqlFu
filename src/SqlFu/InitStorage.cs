ca4571fa0b31881899ebe05d343c2ae41c86d722
Mihai Mogosanu <mike@sapiensworks.com>
2017-10-01 09:52:28 -0400

    changed storage creator helpers

--- src/SqlFu/InitStorage.cs
+++ src/SqlFu/InitStorage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using SqlFu.Providers;
 
@@ -6,14 +7,36 @@ namespace SqlFu
 {
     public static class InitStorage
     {
-        public static void CreateStorage(this DbConnection cnx, Func<IEscapeIdentifier, string> getSql,
-            Action<SqlFuConfig> config = null)
-            => CreateStorage(cnx, getSql(cnx.Provider()), config);
       
-        public static void CreateStorage(this DbConnection cnx,string sql,Action<SqlFuConfig> config=null)
+        public static void CreateStorage(this DbConnection cnx,Action<ICreateForProviders> provSql,Action<SqlFuConfig> config=null)
         {
+            var reg=new SqlForProviders();
+            provSql(reg);
+            var sql = reg.GetSqlForProvider(cnx.Provider());
             config?.Invoke(cnx.SqlFuConfig());
             cnx.AddDbObjectOrIgnore(sql);
+        }
+    }
+
+    public interface ICreateForProviders
+    {
+        ICreateForProviders When<T>(Func<IEscapeIdentifier,string> getSql) where T : IDbProvider;
+    }
+
+    public class SqlForProviders : Dictionary<Type, Func<IEscapeIdentifier, string>>, ICreateForProviders
+    {
+        public string GetSqlForProvider(IDbProvider prov)
+        {
+            var func = this.GetValueOrDefault(prov.GetType());
+            if(func==null) throw new InvalidOperationException($"No sql is specified for provider {prov.ProviderId}");
+            return func(prov);
+        }
+
+        public ICreateForProviders When<T>(Func<IEscapeIdentifier, string> getSql) where T : IDbProvider
+        {
+            getSql.MustNotBeNull();
+            this[typeof(T)] = getSql;
+            return this;
         }
     }
 }
