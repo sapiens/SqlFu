d2625ffd3320eef6f2c9846672f516e252caebd1
Mike <mike@sapiensworks.com>
2020-09-24 13:19:30 -0400

    cleanup

--- src/SqlFu/Mapping/Internals/Mapper.cs
+++ src/SqlFu/Mapping/Internals/Mapper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using SqlFu.Configuration.Internals;
 
 namespace SqlFu.Mapping.Internals
